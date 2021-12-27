742f49f51cbb102df4d0ec523404a9b57cf0296d
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-27 13:26:04 +0530

    COMPLETED:- Sequence practise problems using java-script.

---
+++ sequence-practise-problems/AddTwoRandomNums_3.js
@@ -0,0 +1,5 @@
+let rand1 = Math.floor(Math.random()*6+1);
+let rand2 = Math.floor(Math.random()*6+1);
+console.log("Random 1 :- " + rand1);
+console.log("Random 2 :- " + rand2);
+console.log("ADDITION :- " + (rand1+rand2));
---
+++ sequence-practise-problems/FiveRandTwoDigit_4.js
@@ -0,0 +1,10 @@
+let rand1 = Math.floor(Math.random()*90+10);
+let rand2 = Math.floor(Math.random()*90+10);
+let rand3 = Math.floor(Math.random()*90+10);
+let rand4 = Math.floor(Math.random()*90+10);
+let rand5 = Math.floor(Math.random()*90+10);
+
+let sum = rand1+rand2+rand3+rand4+rand5;
+let avg = sum/5;
+
+console.log("Sum :- " + sum + "\n" + "Average :- " + avg);
